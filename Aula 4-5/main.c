a600867c781c67f9a27f83ad755c096057c0da57
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-04-30 10:34:18 -0300

    Aula 4-5
    https://www.youtube.com/watch?v=8ISwAYeZZ_E&list=PLa75BYTPDNKZWYypgOFEsX3H2Mg-SzuLW&index=4

---
+++ Aula 4-5/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int minhaIdade;
+//Criei uma variavel
+
+    minhaIdade =23;
+//Chamei a variavel e atribui um valor
+
+    printf ("Minha idade é = 23\n");
+//Mandei imprimir a variavel 
+
+    return 0;
+
+}
---
+++ main2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int minhaIdade;
+//Criei uma variavel
+
+    minhaIdade =23;
+//Chamei a variavel e atribui um valor
+
+    printf ("Minha idade e = %i.\n",|minhaIdade);
+//Imprimir a variavel de tipo int e o valor
+
+    return 0;
+
+}
