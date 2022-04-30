0e1928bc5499703479ba6cd745fc3c3b6ec38c0c
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-04-30 10:53:47 -0300

    Atualização
    https://www.youtube.com/watch?v=LenaoK4S23M&list=PLa75BYTPDNKZWYypgOFEsX3H2Mg-SzuLW&index=5

---
+++ main3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int minha Idade;
+    minhaIdade = 23;
+    
+    int maeIdade = 48;
+
+    int paiIdade = 49;
+
+    printf ("Minha idade = %i\nPai idade = %i\n Mae idade = %i\n",
+    minha idade, paiIdade, maeIdade);
+
+    retun 0;
+}
